6c69ff0dd6da6da7516077688a062f7ef3043015
majdsassi <108025873+majdsassi@users.noreply.github.com>
2023-04-06 18:44:15 +0100

    Update email sender.py

--- email sender.py
+++ email sender.py
@@ -8,10 +8,10 @@ configuration = sib_api_v3_sdk.Configuration()
 configuration.api_key['api-key'] = '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'
 
 api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
-subject = "TRES IMPORTANT AYECH WELDI "
-html_content = "<html><body><h1>chawechi ayech weldi godwaa t3adeli li salle 21 bch n3melek chicha fil 5fif tak tak </h1></body></html>"
-sender = {"name":"Amel Dridi ","email":"ameldridi@gmail.com"}
-to = [{"email":"jackreider997@gmail.com","name":"moomen chouachi"}]
+subject = "hne subject "
+html_content = "<html><body><h1hne tekteb el email t7eb tab3thou </h1></body></html>"
+sender = {"name":"hne el name eli t7ebou yodher ","email":"hne t7ot el mail eli t7b yodher 3la asses b3thet meno"}
+to = [{"email":"hne el bch tb3thlou el mail","name":"hne esmou"}]
 headers = {"Some-Custom-Name":"unique-id-1234"}
 params = {"parameter":"My param value","subject":"devoir"}
 send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, headers=headers, html_content=html_content, sender=sender, subject=subject)
